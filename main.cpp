33e0a281bd811550fde576a84c2e9beb1176bfb8
S5640603 <115167282+S5640603@users.noreply.github.com>
2022-11-23 17:38:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+      int i, n, primo=1, secondo=1, terzo;
+
+    do {
+        cin>>n;
+        if (n<2)
+            cout<<"errore";
+    } while (n<2);
+    cout<<primo<<endl<<secondo<<endl;
+
+    for(i=2;i<n;i++){
+        terzo=primo+secondo;
+        primo=secondo;
+        secondo=terzo;
+        cout<<terzo<<endl;
+    }
    return 0;
 }
